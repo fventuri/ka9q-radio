2869bb5adb319f663e60cefa100e3dcdb78b9396
Franco Venturi <fventuri@comcast.net>
2023-05-20 14:13:56 -0400

    set Si5351 registries directly for a better control of the sample rate and reference oscillator frequency

--- rx888d.c
+++ rx888d.c
@@ -76,7 +76,8 @@ struct sdrstate {
   int xfers_in_progress;
 
   char const *description;
-  unsigned int samprate; // True sample rate of single A/D converter
+  double samprate;  // True sample rate of single A/D converter
+  double reference; // reference oscillator frequency
 
   // Hardware
   bool randomizer;
@@ -133,13 +134,15 @@ static int rx888_init(struct sdrstate *sdr,const char *firmware,unsigned int que
 static void rx888_set_dither_and_randomizer(struct sdrstate *sdr,bool dither,bool randomizer);
 static void rx888_set_att(struct sdrstate *sdr,float att);
 static void rx888_set_gain(struct sdrstate *sdr,float gain);
-static void rx888_set_samprate(struct sdrstate *sdr,unsigned int samprate);
+static void rx888_set_samprate(struct sdrstate *sdr,double samprate,double reference);
 static int rx888_start_rx(struct sdrstate *sdr,libusb_transfer_cb_fn callback);
 static void rx888_stop_rx(struct sdrstate *sdr);
 static void rx888_close(struct sdrstate *sdr);
 static void free_transfer_buffers(unsigned char **databuffers,struct libusb_transfer **transfers,unsigned int queuedepth);
 static double val2gain(int g);
 static int gain2val(int m, double gain);
+static void rational_approximation(double value, uint32_t max_denominator, uint32_t *a, uint32_t *b, uint32_t *c);
+
 
 dictionary *Dictionary;
 char const *Name;
@@ -317,17 +320,19 @@ int main(int argc,char *argv[]){
     rx888_set_gain(sdr,gain);
 
     // Sample Rate, default 32000000 (32 MHz)
-    unsigned int samprate = config_getint(Dictionary,Name,"samprate",32000000);
-    if(samprate < 1000000){
-      int const minsamprate = 1000000; // 1 MHz?
-      fprintf(stdout,"Invalid sample rate %'d, forcing %'d\n",samprate,minsamprate);
+    double samprate = config_getdouble(Dictionary,Name,"samprate",32000000.0);
+    double const minsamprate = 1000000.0; // 1 MHz?
+    if(samprate < minsamprate){
+      fprintf(stdout,"Invalid sample rate %'lf, forcing %'lf\n",samprate,minsamprate);
       samprate = minsamprate;
     }
-    rx888_set_samprate(sdr,samprate);
+    // Reference oscillator frequency, default 27000000 (27 MHz)
+    double const reference = config_getdouble(Dictionary,Name,"reference",27000000.0);
+    rx888_set_samprate(sdr,samprate,reference);
   }
   
-  fprintf(stdout,"Samprate %'d, Gain %.1f dB, Attenuation %.1f dB, Dithering %d, Randomizer %d, USB Queue depth %d, USB Request size %d * pktsize %d = %'d bytes\n",
-	  sdr->samprate,sdr->rf_gain,sdr->rf_atten,sdr->dither,sdr->randomizer,sdr->queuedepth,sdr->reqsize,sdr->pktsize,sdr->reqsize * sdr->pktsize);
+  fprintf(stdout,"Samprate %'lf, Reference %'lf, Gain %.1f dB, Attenuation %.1f dB, Dithering %d, Randomizer %d, USB Queue depth %d, USB Request size %d * pktsize %d = %'d bytes\n",
+	  sdr->samprate,sdr->reference,sdr->rf_gain,sdr->rf_atten,sdr->dither,sdr->randomizer,sdr->queuedepth,sdr->reqsize,sdr->pktsize,sdr->reqsize * sdr->pktsize);
 
   // When the IP TTL is 0, we're not limited by the Ethernet hardware MTU so select a much larger packet size
   // unless one has been set explicitly
@@ -857,10 +862,96 @@ static void rx888_set_gain(struct sdrstate *sdr,float gain){
   sdr->rf_gain = val2gain(arg); // Store actual nearest value
 }
 
-static void rx888_set_samprate(struct sdrstate *sdr,unsigned int samprate){
+// see: SiLabs Application Note AN619 - Manually Generating an Si5351 Register Map (https://www.silabs.com/documents/public/application-notes/AN619.pdf)
+static void rx888_set_samprate(struct sdrstate *sdr,double samprate,double reference){
   usleep(5000);
-  command_send(sdr->dev_handle,STARTADC,samprate);
+  if(samprate == 0){
+    // power off clock 0
+    control_send_byte(sdr->dev_handle,I2CWFX3,SI5351_ADDR,SI5351_REGISTER_CLK_BASE+0,SI5351_VALUE_CLK_PDN);
+    return;
+  }
+
+  /* if the requested sample rate is below 1MHz, use an R divider */
+  double r_samprate = samprate;
+  uint8_t rdiv = 0;
+  while (r_samprate < 1e6 && rdiv <= 7) {
+    r_samprate *= 2.0;
+    rdiv += 1;
+  }
+  if (r_samprate < 1e6) {
+    fprintf(stdout,"ERROR - requested sample rate is too low: %'lg\n",samprate);
+    return;
+  }
+
+  /* choose an even integer for the output MS */
+  uint32_t output_ms = ((uint32_t)(SI5351_MAX_VCO_FREQ / r_samprate));
+  output_ms -= output_ms % 2;
+  if (output_ms < 4 || output_ms > 2048) {
+    fprintf(stdout,"ERROR - invalid output MS: %d  (samprate=%lg)\n",output_ms,samprate);
+    return;
+  }
+  double const vco_frequency = r_samprate * output_ms;
+
+  /* feedback MS */
+  double const feedback_ms = vco_frequency / reference;
+  /* find a good rational approximation for feedback_ms */
+  uint32_t a;
+  uint32_t b;
+  uint32_t c;
+  rational_approximation(feedback_ms,SI5351_MAX_DENOMINATOR,&a,&b,&c);
+
+  double const actual_ratio = a + (double)b / (double)c;
+  double const actual_samprate = reference * actual_ratio / output_ms / pow(2.0,rdiv);
+  fprintf(stdout,"Samprate %'lf, Reference %'lf, A=%d B=%d C=%d output_ms=%d rdice=%d Actual Samprate %'lf\n",
+	  sdr->samprate,sdr->reference,a,b,c,output_ms,rdiv,actual_samprate);
+
+  /* configure clock input and PLL */
+  uint32_t const b_over_c = 128 * b / c;
+  uint32_t const msn_p1 = 128 * a + b_over_c - 512;
+  uint32_t const msn_p2 = 128 * b  - c * b_over_c;
+  uint32_t const msn_p3 = c;
+
+  uint8_t data_clkin[] = {
+    (msn_p3 & 0x0000ff00) >>  8,
+    (msn_p3 & 0x000000ff) >>  0,
+    (msn_p1 & 0x00030000) >> 16,
+    (msn_p1 & 0x0000ff00) >>  8,
+    (msn_p1 & 0x000000ff) >>  0,
+    (msn_p3 & 0x000f0000) >> 12 | (msn_p2 & 0x000f0000) >> 16,
+    (msn_p2 & 0x0000ff00) >>  8,
+    (msn_p2 & 0x000000ff) >>  0
+  };
+
+  control_send(sdr->dev_handle,I2CWFX3,SI5351_ADDR,SI5351_REGISTER_MSNA_BASE,data_clkin,sizeof(data_clkin));
+
+  /* configure clock output */
+  /* since the output divider is an even integer a = output_ms, b = 0, c = 1 */
+  uint32_t const ms_p1 = 128 * output_ms - 512;
+  uint32_t const ms_p2 = 0;
+  uint32_t const ms_p3 = 1;
+
+  uint8_t data_clkout[] = {
+    (ms_p3 & 0x0000ff00) >>  8,
+    (ms_p3 & 0x000000ff) >>  0,
+    rdiv << 5 | (ms_p1 & 0x00030000) >> 16,
+    (ms_p1 & 0x0000ff00) >>  8,
+    (ms_p1 & 0x000000ff) >>  0,
+    (ms_p3 & 0x000f0000) >> 12 | (ms_p2 & 0x000f0000) >> 16,
+    (ms_p2 & 0x0000ff00) >>  8,
+    (ms_p2 & 0x000000ff) >>  0
+  };
+
+  control_send(sdr->dev_handle,I2CWFX3,SI5351_ADDR,SI5351_REGISTER_MS0_BASE,data_clkout,sizeof(data_clkout));
+
+  // start clock
+  control_send_byte(sdr->dev_handle,I2CWFX3,SI5351_ADDR,SI5351_REGISTER_PLL_RESET,SI5351_VALUE_PLLA_RESET);
+  // power on clock 0
+  uint8_t const clock_control = SI5351_VALUE_MS_INT | SI5351_VALUE_CLK_SRC_MS | SI5351_VALUE_CLK_DRV_8MA | SI5351_VALUE_MS_SRC_PLLA;
+  control_send_byte(sdr->dev_handle,I2CWFX3,SI5351_ADDR,SI5351_REGISTER_CLK_BASE+0,clock_control);
+
+  usleep(1000);
   sdr->samprate = samprate;
+  sdr->reference = reference;
 }
 
 static int rx888_start_rx(struct sdrstate *sdr,libusb_transfer_cb_fn callback){
@@ -967,4 +1058,54 @@ static int gain2val(int m, double gain){
     g = 0;
   g |= (m << 7);
   return g;
+}
+
+/* best rational approximation:
+ *
+ *     value ~= a + b/c     (where c <= max_denominator)
+ *
+ * References:
+ * - https://en.wikipedia.org/wiki/Continued_fraction#Best_rational_approximations
+ */
+static void rational_approximation(double value, uint32_t max_denominator,
+                                   uint32_t *a, uint32_t *b, uint32_t *c)
+{
+  const double epsilon = 1e-5;
+
+  double af;
+  double f0 = modf(value, &af);
+  *a = (uint32_t) af;
+  *b = 0;
+  *c = 1;
+  double f = f0;
+  double delta = f0;
+  /* we need to take into account that the fractional part has a_0 = 0 */
+  uint32_t h[] = {1, 0};
+  uint32_t k[] = {0, 1};
+  for(int i = 0; i < 100; ++i){
+    if(f <= epsilon){
+      break;
+    }
+    double anf;
+    f = modf(1.0 / f,&anf);
+    uint32_t an = (uint32_t) anf;
+    for(uint32_t m = (an + 1) / 2; m <= an; ++m){
+      uint32_t hm = m * h[1] + h[0];
+      uint32_t km = m * k[1] + k[0];
+      if(km > max_denominator){
+        break;
+      }
+      double d = fabs((double) hm / (double) km - f0);
+      if(d < delta){
+        delta = d;
+        *b = hm;
+        *c = km;
+      }
+    }
+    uint32_t hn = an * h[1] + h[0];
+    uint32_t kn = an * k[1] + k[0];
+    h[0] = h[1]; h[1] = hn;
+    k[0] = k[1]; k[1] = kn;
+  }
+  return;
 }
